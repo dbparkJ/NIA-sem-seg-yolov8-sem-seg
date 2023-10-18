ed568208cf9e021f9a5f56c93d94dc7d701608cd
dbparkJ <pjmsm0319@gmail.com>
2023-10-18 16:20:00 +0900

    add file_index.txt add train.py add HW,SW information

--- train_model/train.py
+++ train_model/train.py
@@ -1,10 +1,11 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8l-seg.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8n-seg.pt')  # load a pretrained model (recommended for training)
 if __name__ == '__main__':
     model.train(
         data=r'D:\data\model.yaml',
-        epochs=100,
-        batch=8,
-        workers=24
+        epochs=30,
+        batch=32,
+        device=0,
+        name='AI_HUB_yolov8n-seg'
     )
